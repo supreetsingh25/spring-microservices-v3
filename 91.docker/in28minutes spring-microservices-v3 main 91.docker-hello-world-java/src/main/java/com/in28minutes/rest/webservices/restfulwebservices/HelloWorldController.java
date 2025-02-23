c0e9a106c707b16781166dfe744a90a507928d33
supreetsingh <supreetsingh@localhost>
2025-02-23 10:53:49 -0500

    docker

--- 91.docker/in28minutes spring-microservices-v3 main 91.docker-hello-world-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ 91.docker/in28minutes spring-microservices-v3 main 91.docker-hello-world-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping(path = "/")
-    public String helloWorld() {
+	@GetMapping(path = "/")
+	public String helloWorld() {
 		// Implemented using String Templates
-        return """
-                { message: Hello World Java v1 }
-               """;
-    }
+		return """
+				 { message: Hello World Java v4 }
+				""";
+	}
 }
